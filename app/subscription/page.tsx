f7e87887db6ae3efed1475bd50eec838c738d1d8
Guilherme Guidetti <53499339+guilhermeguidetti@users.noreply.github.com>
2025-01-08 10:40:49 -0300

    feat: add authentication check and redirect for Subscription and Transactions pages

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default function SubscriptionPage() {
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/login");
+  }
   return (
     <>
       <Navbar />
