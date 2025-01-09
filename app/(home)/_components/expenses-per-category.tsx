3db7b7591cd3bc5c2f5d5179b1d88573f4dbaa0d
Guilherme Guidetti <53499339+guilhermeguidetti@users.noreply.github.com>
2025-01-09 10:17:22 -0300

    feat: add expenses per category component

---
+++ app/(home)/_components/expenses-per-category.tsx
@@ -0,0 +1,41 @@
+import { CardContent, CardHeader, CardTitle } from "@/app/_components/ui/card";
+import { Progress } from "@/app/_components/ui/progress";
+import { ScrollArea } from "@/app/_components/ui/scroll-area";
+import { TRANSACTION_CATEGORY_LABELS } from "@/app/_constants/transaction";
+import { TotalExpensePerCategory } from "@/app/_data/get-dashboard/types";
+
+interface ExpensesPerCategoryProps {
+  expensesPerCategory: TotalExpensePerCategory[];
+}
+
+const ExpensesPerCategory = ({
+  expensesPerCategory,
+}: ExpensesPerCategoryProps) => {
+  return (
+    <ScrollArea className="pB-6 col-span-2 h-full rounded-md border">
+      <CardHeader>
+        <CardTitle className="font-bold">Despesas por categoria</CardTitle>
+      </CardHeader>
+
+      <CardContent className="space-y-6">
+        {expensesPerCategory.map((category) => (
+          <div key={category.category}>
+            <div className="space-y-2">
+              <div className="flex w-full justify-between">
+                <p className="text-sm font-bold">
+                  {TRANSACTION_CATEGORY_LABELS[category.category]}
+                </p>
+                <p className="text-sm font-bold">
+                  {category.percentageOfTotal}%
+                </p>
+              </div>
+              <Progress value={category.percentageOfTotal} />
+            </div>
+          </div>
+        ))}
+      </CardContent>
+    </ScrollArea>
+  );
+};
+
+export default ExpensesPerCategory;
