7c815ac244520d182246e5af06c43c326466e4fa
Guilherme Guidetti <53499339+guilhermeguidetti@users.noreply.github.com>
2025-01-09 10:05:14 -0300

    feat: add chart

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,6 +4,8 @@ import Navbar from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
 import TimeSelect from "./_components/time-select";
 import { isMatch } from "date-fns";
+import TransactionsPieChart from "./_components/transactions-pie-chart";
+import { getDashboard } from "../_data/get-dashboard";
 
 interface HomeProps {
   searchParams: {
@@ -11,7 +13,7 @@ interface HomeProps {
   };
 }
 
-const Home = ({ searchParams: { month } }: HomeProps) => {
+const Home = async ({ searchParams: { month } }: HomeProps) => {
   const { userId } = auth();
   if (!userId) {
     redirect("/login");
@@ -22,6 +24,9 @@ const Home = ({ searchParams: { month } }: HomeProps) => {
     redirect("/?month=01");
   }
 
+  const dashboard = await getDashboard(month);
+  console.log(dashboard);
+
   return (
     <>
       <Navbar />
@@ -30,8 +35,15 @@ const Home = ({ searchParams: { month } }: HomeProps) => {
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
+        <div className="grid grid-cols-[2fr,1fr]">
+          <div className="flex flex-col gap-6">
+            <SummaryCards {...dashboard} />
+            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+              <TransactionsPieChart {...dashboard} />
+            </div>
+          </div>
+        </div>
       </div>
-      <SummaryCards month={month} />
     </>
   );
 };
